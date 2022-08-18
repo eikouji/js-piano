be20a8ccea5cfd7960282c6a2bcecb381460c60f
Eiko Ujifusa <eikorouji@gmail.com>
2022-08-18 11:43:59 -0700

    changed note divs and class names

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-const pianoKeys = ['',]
+const pianoKeys = [
+    'c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3', 'c4',
+    'c#3', 'd#3', 'f#3', 'g#3', 'a#3'];
+    const notes = [];
+    keys.forEach(function(key){
+        notes.push(document.getElementById(key));
+    })
+
+
+    // change the color of the note when clicked //
+    function keyPlay(e){
+        if (e.target.className === "keynote" || e.target.className === "black-keynote" ){
+            e.target.parentElement.style.backgroundColor = "yellow";
+
+        }else{
+            e.target.style.backgroundColor = "yellow";
+        }
+        playAudio(e);
+    }
+
+    function keyReturn(e) {
+        e.target.style.backgroundColor = "";
+        e.target.parentElement.style.backgroundColor = "";
+    }
+
+    function playAudio(e){
+        if(e.target.className === "keynote" || e.target.className === 
+        black-keynote"){
+            
+        }
+    }
+
+
